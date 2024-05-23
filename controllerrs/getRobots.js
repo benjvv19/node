92b151ef5d338a73a2ca29f029fd3f6f85154a50
benjvv19 <ben.camus.19@gmail.com>
2024-05-23 10:10:30 -0400

    primer commit

---
+++ controllerrs/getRobots.js
@@ -0,0 +1,29 @@
+
+
+
+
+const {request , response} = require("express");
+const { robots } = require ("../robots");
+
+const getRobots =(req= request , res=response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports={getRobots};
